ea5b67f3f0ff07395958531ffe28c1ccb5db5030
mamoune <adi.mamoune@gmail.com>
2018-12-18 17:40:13 +0100

    first commit du projet

---
+++ IdeaProjects/IdeaProjects/Project_Java/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util.println;
+
+public class Main {
+
+    public static void main(String[] args) {
+        println("salut tt le monde");
+    }
+}
