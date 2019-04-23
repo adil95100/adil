6e70440a8306edded0a5dd1014c2e30ed1722845
adil95100 <adil.mamoune@gmail.com>
2019-04-23 22:27:12 +0200

    rajout des fonctions dans le script shell

--- IdeaProjects/script_test.sh
+++ IdeaProjects/script_test.sh
@@ -1,12 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 # Make a copy of the file 
 
-function LisezMoi(){
+noir='\e[0;30m'
+gris='\e[1;30m'
+rougefonce='\e[0;31m'
+rose='\e[1;31m'
+vertfonce='\e[0;32m'
+vertclair='\e[1;32m'
+orange='\e[0;33m'
+jaune='\e[1;33m'
+bleufonce='\e[0;34m'
+bleuclair='\e[1;34m'
+violetfonce='\e[0;35m'
+violetclair='\e[1;35m'
+cyanfonce='\e[0;36m'
+cyanclair='\e[1;36m'
+grisclair='\e[0;37m'
+blanc='\e[1;37m'
+neutre='\e[0;m'
+
+tab=("$noir" "$gris" "$rougefonce" "$rose" "$bleufonce" "$vertfonce")
+LisezMoi()
+{
 for i in {0..5}
 do
-echo -e "======================================================================"
+echo -e "${tab[i]}==================================================== ${neutre}"
 echo -e "========Bienvenu Dans le SCript Shell pour utiliser les fonctions====="
-echo -e "======================================================================="
+echo -e "${tab[i]}==================================================== ${neutre}"
+	
 done
 }
 
@@ -21,10 +42,3 @@ else
         echo "le fichier makayenche les amis ??????????????"
 	echo "enfin c'est juste un petit test"
 fi
-echo "nom de script=$0"
-if ($(who | grep adil|wc -l) -ge 1)
-then
-  echo " adil est connecté"
-else
-      " adil n'est pas connecté"	
-fi  
